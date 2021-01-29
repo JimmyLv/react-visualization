52c48ab41869d2c6718f536d5fe7e0b94f9dd63f
JimmyLv <jimmy.jinglv@gmail.com>
2021-01-30 00:26:36 +0800

    added echarts render

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,52 @@
+import React, { useEffect, useRef } from 'react';
+import * as echarts from 'echarts';
 import styles from './index.css';
 
 export default function() {
+  const chartRef = useRef();
+  let myChart = null;
+  const options = {
+    title: {
+      text: '测试图表-React-Hook',
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+      },
+    ],
+  };
+
+  function renderChart() {
+    const chart = echarts.getInstanceByDom(chartRef.current);
+    if (chart) {
+      myChart = chart;
+    } else {
+      myChart = echarts.init(chartRef.current);
+    }
+    myChart.setOption(options);
+  }
+
+  useEffect(() => {
+    renderChart();
+    return () => {
+      myChart && myChart.dispose();
+    };
+  });
+
   return (
     <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
+      <div style={{ width: '400px', height: '400px' }} ref={chartRef} />
     </div>
   );
 }
